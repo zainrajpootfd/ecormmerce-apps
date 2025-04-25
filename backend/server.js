20ee08d92289931b1effcc8aff2dede4f7faba95
zain <zainrajpootfd@gmail.com>
2025-04-25 12:28:43 +0500

    modify api endpoints

--- backend/server.js
+++ backend/server.js
@@ -7,6 +7,9 @@ const productRouter = require("./routes/productRoute");
 const cartRouter = require("./routes/cartRoute");
 const orderRouter = require("./routes/orderRoute");
 const subscriberRouter = require("./routes/subscribeRoute");
+const shopRouter = require("./routes/shopRoute");
+const InventoryRouter = require("./routes/inventoryRoute");
+const transactionRouter = require("./routes/transactionRoute");
 
 const app = express();
 const Port = process.env.PORT || 4000;
@@ -29,6 +32,9 @@ app.use("/api/product", productRouter);
 app.use("/api/cart", cartRouter);
 app.use("/api/order", orderRouter);
 app.use("/api/user", subscriberRouter);
+app.use("/api/shops", shopRouter);
+app.use("/api/inventory", InventoryRouter);
+app.use("/api/transactions", transactionRouter);
 
 // Start server
 app.listen(Port, () => {
